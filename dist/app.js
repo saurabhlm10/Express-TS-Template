06585913a9a27c8ef69801da976e73721930acdf
Saurabh <ssuryavanshi@ineuron.ai>
2023-11-11 13:33:29 +0530

    first commit

---
+++ dist/app.js
@@ -0,0 +1,14 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const express_1 = __importDefault(require("express"));
+const db_1 = __importDefault(require("./config/db"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+(0, db_1.default)();
+exports.default = app;
---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+import express from "express";
+import connectToDb from "./config/db";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+connectToDb();
+
+export default app;
